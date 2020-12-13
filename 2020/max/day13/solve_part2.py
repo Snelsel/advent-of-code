13db216d9fe0c54d7977c69eb1c34602c6be3360
maxhaventon <max@haventon.se>
2020-12-13 23:43:50 +0100

    Semi-brute approach with merge of bus lines

--- 2020/max/day13/solve_part2.py
+++ 2020/max/day13/solve_part2.py
@@ -1,44 +1,63 @@
 from operator import itemgetter
 
+class Bus:
+    def __init__(self, k, m):
+        self.k = k
+        self.m = m
+    
+    def is_valid(self, t):
+        return t % self.k - self.m == 0
+
+    def merge(self, other):
+        x = 0
+        matches = []
+        while True:
+            # if x > 1000000000:
+            #     raise Exception('Possible infinite loop detected')
+            
+            t = self.k * x + self.m
+
+            # if not self.is_valid(t):
+            #     raise Exception('Dont try a t thats not valid for yourself...')
+
+            if other.is_valid(t):
+                matches.append(t)
+
+                if len(matches) > 2:
+                    k = matches[1] - matches[0]
+                    m = matches[0]
+                    bus = Bus(k, m)
+                    # for match in matches:
+                    #     if not bus.is_valid(t):
+                    #         print(matches, bus)
+                    #         raise Exception('Failed merge!')
+                    return bus
+            
+            x += 1
+
+    def first_t(self):
+        return self.k - self.m
+
+    def __str__(self):
+        return f'Bus with period {self.k} and offset {self.m}'
+
 def parse(input):
     buses = []
     for i, c in enumerate(input.split(',')):
         if c != 'x':
-            buses.append((i, int(c)))
+            buses.append(Bus(int(c), i))
     return buses
 
 def process(buses):
-    sorted_buses = sorted(buses, key=itemgetter(1))
-    
-    bus_with_highest_k = sorted_buses.pop()
-
-    sorted_buses = sorted(buses, key=itemgetter(1), reverse=True)
-
-    offset = bus_with_highest_k[0]
-    k = bus_with_highest_k[1]
-
-    i = 0
-    while True:
-        if i > 100000000000:
-            raise Exception('Infinite loop')
-
-        t = i * k - offset
-        if valid(t, sorted_buses):
-            return t
+    i = 1
+    new_bus = None
+    print(len(buses) - 1)
+    while i <= (len(buses) - 1):
+        old_bus = buses[i-1] if new_bus is None else new_bus
+        new_bus = old_bus.merge(buses[i])
+        print(i, new_bus)
         i += 1
+    return new_bus.first_t()
 
-def valid(t, buses):
-    for bus in buses:
-        if (t + bus[0]) % bus[1] != 0:
-            return False
-    return True
-
-
-print(3417, process(parse('17,x,13,19')) == 3417)
-print(754018, process(parse('67,7,59,61')) == 754018)
-print(779210, process(parse('67,x,7,59,61')) == 779210)
-print(1261476, process(parse('67,7,x,59,61')) == 1261476)
-print(1202161486, process(parse('1789,37,47,1889')) == 1202161486)
 buses = parse('23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,733,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,19,x,x,x,x,x,x,x,x,x,29,x,449,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37')
-print(buses)
 print(process(buses))
